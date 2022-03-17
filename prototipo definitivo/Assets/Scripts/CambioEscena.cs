5b45d86e92d631f9fd5e4c9a52a813df5e19aa04
Key2Board <81231376+Key2Board@users.noreply.github.com>
2022-03-17 22:21:25 +0100

    Contenido
    Se ha añadido un menú principal para poder jugar al juego, un sistema de cambio de niveles y se ha modificado el color de el gancho y el suelo

---
+++ prototipo definitivo/Assets/Scripts/CambioEscena.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioEscena : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ prototipo definitivo/Assets/Scripts/Trigger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    bool triggered = false;
+
+    public AudioSource Sonido;
+
+    public void PlaySonido()
+    {
+        Sonido.Play();
+    }
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (triggered != true)
+        {
+            PlaySonido();
+            triggered = true;
+        }
+    }
+}
