6101c6a489c144c3501bfe8b4948bb2cd91051e6
Key2Board <81231376+Key2Board@users.noreply.github.com>
2022-03-21 22:56:25 +0100

    Modificaciones finales
    Se han hecho modificaciones al terreno, se ha añadido skyboxes, se ha añadido sonidos y adiciones extras

---
+++ prototipo definitivo/Assets/Scripts/ActivarCursor.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivarCursor : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+}
