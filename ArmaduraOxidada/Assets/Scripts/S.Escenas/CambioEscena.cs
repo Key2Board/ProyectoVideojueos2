26e386804886fa59ca5bb4fe2c4fcbee98043ce0
Key2Board <81231376+Key2Board@users.noreply.github.com>
2022-04-17 19:40:46 +0200

    Commit_Fran_5
    Se ha realizado mejoras a los blockouts, y se ha comenzado con transiciones entre escenas y abrir puertas

--- ArmaduraOxidada/Assets/Scripts/S.Escenas/CambioEscena.cs
+++ ArmaduraOxidada/Assets/Scripts/S.Escenas/CambioEscena.cs
@@ -5,10 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class CambioEscena : MonoBehaviour
 {
+    //public Animator transition;
+    //public float transitionTime = 1f;
     public int numeroEscena = 0;
     void OnTriggerEnter(Collider collider)
     {
         SceneManager.LoadScene(numeroEscena);
+        //StartCoroutine(LoadLevel(numeroEscena));
     }
+    /*IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(numeroEscena);
+    }*/
 }      
 
